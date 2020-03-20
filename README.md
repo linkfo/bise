f95544923960392ebba4ecf181290ec2b44e7292
blue_Flare <linkmek@hotmail.com>
2020-03-20 17:43:48 +0800

    k

--- README.md
+++
@@ -1,8 +0,0 @@
-# 关于
-整合SSM框架（SpringMVC + Spring + MyBatis)
-＃测试
-this is test
-
-
-#自主建立文件夹
-
---
+++ k/CrabConstant.java
@@ -0,0 +1,18 @@
+package com.baomidou.crab.common;
+
+/**
+ * <p>
+ * 系统常量
+ * </p>
+ *
+ * @author jobob
+ * @since 2018-10-31
+ */
+public interface CrabConstant {
+
+    /**
+     * 默认密码
+     */
+    String DEFAULT_PASSWORD = "100607";
+
+}
